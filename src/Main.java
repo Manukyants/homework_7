837b8d7f4e72b24ae979fecefadc7e4d89b78baa
Манукянц Бабкен <b.manukyants@gmail.com>
2022-07-27 14:38:45 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание №1
+        String middleName = "Иванов ";
+        String firstName = "Семён ";
+        String lastName = "Семёнович ";
+        String fullName = middleName + firstName + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // Задание №2
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - "+fullNameUpper);
+
+        // Задание №3
+        fullName = fullName.replace("ё", "е");
+        fullName = fullName.replace("Ё", "Е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
     }
 }
